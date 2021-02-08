10f9c0cfc8b1cdcb65d062f7e2cdc06ddab47213
Emmy <emerentia.arjani@gmail.com>
2021-02-08 13:31:02 +0700

    deleted friendship model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_134009) do
+ActiveRecord::Schema.define(version: 2021_02_08_062749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,6 @@ ActiveRecord::Schema.define(version: 2021_02_04_134009) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "status", default: false
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer "friend_a_id"
-    t.integer "friend_b_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
