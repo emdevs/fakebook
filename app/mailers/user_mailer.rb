ab81ebab5ea91ef65a30d7d8971a1db36c45ddcc
Emmy <emerentia.arjani@gmail.com>
2021-02-09 12:24:09 +0700

    edited mailer and welcome email view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: "notifications@fakebook.com"
+
+    def welcome_email
+        @user = current_user
+        @url = "http://localhost:3000/"
+        mail(to: @user.email, subject: "Welcome to Fakebook!")
+    end
 end
