2af93d77439f79444f59e5150fb89a2caea9bd92
Emmy <emerentia.arjani@gmail.com>
2021-02-09 13:14:59 +0700

    configured devise registration controller and welcome mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
     def index
         # @users = User.all
-        
         #those whom you havent sent a request, are waiting on a request for, or are already friends with
-
         @friend_request = current_user.sent_friend_requests.build
     end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
     default from: "notifications@fakebook.com"
 
-    def welcome_email
-        @user = current_user
+    def welcome_email(params)
+        @user = params[:user]
         @url = "http://localhost:3000/"
-        mail(to: @user.email, subject: "Welcome to Fakebook!")
+        mail(to: @user[:email], subject: "Welcome to Fakebook!")
     end
 end
