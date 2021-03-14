e6bd366476d4b3a6d152998d4684573daf058875
Emmy <emerentia.arjani@gmail.com>
2021-03-14 14:51:47 +0700

    fixed an omniauth fb bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,14 +54,12 @@ class User < ApplicationRecord
 
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_initialize.tap do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      # user.profile_pic = auth.info.image
+      user.save
     end
   end
 
@@ -73,8 +71,3 @@ class User < ApplicationRecord
     end
   end
 end
-
-  #friendship part 2
-  # has_many :friendships, ->(user) {unscope(:where).where(friend_a: user).or(where(friend_b: user))}
-
-  
