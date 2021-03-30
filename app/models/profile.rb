76575fed04d0a9dddda555b68e0cf8309467f57f
Emmy <emerentia.arjani@gmail.com>
2021-03-30 13:20:05 +0700

    added default profile pic and d some css styling

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,21 @@
 class Profile < ApplicationRecord
     #attach profile pic
-    has_one_attached :profile_pic, dependent: :destory
+    # before_create :image_nil?
+    include ActionView::Helpers::UrlHelper
+
+    has_one_attached :profile_pic
     belongs_to :user
+
+
+    def attached_img
+        img_path = (self.profile_pic.attached?)? self.profile_pic : "default.png"
+    end
+
+    private
+
+    # def image_nil?
+    #     if !(self.profile_pic)
+    #         self.profile_pic.attach(io: File.open('app/assets/images/default.png'), filename: 'default.png', content_type: 'image/png')
+    #     end
+    # end
 end
