dd374cb94e0d907388c87bda2c20e3cd12bf8397
Emmy <emerentia.arjani@gmail.com>
2021-04-05 18:20:15 +0700

    styled comment form errors, edited club post controller

--- app/controllers/clubs/posts_controller.rb
+++ app/controllers/clubs/posts_controller.rb
@@ -1,6 +1,6 @@
 class Clubs::PostsController < PostsController
     #this will inherit like method on post
     def index
-        @posts = Post.where(postable_type: "Club").order('created_at DESC')
+        @posts = Post.where(postable_type: "Club", postable_id: params[:club_id]).order('created_at DESC')
     end
 end
