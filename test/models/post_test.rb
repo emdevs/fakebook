61aea1f994d20b2edba43a64e2f94739f9706624
Emmy <emerentia.arjani@gmail.com>
2021-09-18 17:59:17 +0700

    fixed fixtures, added user model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  #each post MUST have title, body, user and postable. 
+  #attached image, comments and likes optional. 
+
+  # test "fails if post is blank" do
+  #   post = Post.new
+  #   assert_not post.save, "Saved blank post"
+  # end
+
+  # test "fails if post title is less than 5 characters" do
+  #   post = posts(:one)
+  #   post.title = "less"
+  #   assert_not post.save, "Saved invalid post title"
+  # end
+
+  # test "fails if post body is less than 10 characters" do
+  #   post = posts(:one)
+  #   post.body = "less"
+  #   assert_not post.save, "Saved invalid post body"
+  # end
+
+  # test "fails if post doesn't belong to a user" do
+  #   post = posts(:one)
+  #   post.user = nil
+  #   assert_not post.save, "Saved with nil user"
+  # end
+
+  # test "fails if post doesn't belong to a postable" do
+  #   post = posts(:one)
+  #   post.postable = nil
+  #   assert_not post.save, "Saved with nil postable"
+  # end
+
+
+  # #is it ebcause user and wall doesn't exist yet?
+  # test "passes with valid post" do
+  #   #why is this failing?
+  #   newpost = Post.new(
+  #     title: "MyString",
+  #     body: "MyText that is long enough for body",
+  #     user_id: 1,
+  #     postable_id: 1,
+  #     postable_type: "Wall",
+  #   )
+  #   puts User.first
+  #   # assert posts(:one).save, "Didn't save valid post."
+  #   assert newpost.save, "didnt save valid post"
   # end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  #MANDATORY: email, name, birth_date, encrypted password
+  #OPTIONAL: provider, uid, gender. (right now no way to create user without a gender. )
+
+  test "doesn't save blank user" do
+    user = User.new
+    assert_not user.save, "Saved blank user"
+  end
+
+  test "fails if age is below 13 years old" do
+    user = users(:one)
+    user.birth_date = "21-04-2021"
+    assert_not user.save, "Saved invalid birthdate"
+  end
+
+  test "fails if age is above 120 years old" do
+    user = users(:one)
+    user.birth_date = "21-04-1860"
+    assert_not user.save, "Saved invalid birthdate"
+  end
+
+  # test "fails if gender is blank" do 
+  #   user = users(:one)
+  #   user.gender = nil
+  #   assert_not user.save, "Save blank gender"
   # end
+
+  test "saves a valid user" do 
+    assert users(:one).save, "Didn't save valid user"
+  end
+
 end
